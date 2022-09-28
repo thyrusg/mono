cc29c4ba2696d4c45766f776f5d782ca0a9152fa
Thyrus Gorges <me@thyrusgorges.com>
2022-09-28 14:48:26 -0700

    Add infinite processing lopp
    Infinite loop compares the modification time of a file. If the modification time changes, then we know the file was changed so we execute the provided command.

--- watcher/watcher.rb
+++ watcher/watcher.rb
@@ -4,6 +4,7 @@ require 'optparse'
 
 options = {}
 required_options = [:interval, :file, :command]
+forever = true
 
 def check_required_options(options, required)
   missing = required - options.keys
@@ -34,3 +35,16 @@ end.parse!
 p options
 puts "===== Checking options ====="
 check_required_options(options, required_options)
+
+while forever # Loop forever
+  file = File.new(options[:file])
+  old_mtime = File.mtime(file)
+  puts "Sleeping #{options[:interval]}"
+  sleep options[:interval].to_i
+  new_mtime = File.mtime(file)
+  if new_mtime > old_mtime
+    puts "#{options[:file]} changed. Running command."
+    system(options[:command])
+  end
+  file.close
+end
